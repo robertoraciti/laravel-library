47b04446a8289609c865dc4a81b4e08c83483eca
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-10-20 16:27:30 +0200

    added create controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -21,11 +21,11 @@ class BookController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        
+        return view('books.create');
     }
 
     /**
