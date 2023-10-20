6e219002ab8d2f9a99c799a3ccd30f486a70cfb4
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-10-20 17:22:22 +0300

    style: show

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -2,16 +2,16 @@
 
 @section('main-content')
 
-<div class="container">
+<div class="container mt-5">
+    <h3 class="text-danger"> {{ $book->title }} </h3>
     <div class="row">
-        <div class="col"> {{ $book->title }} </div>
-        <div class="col"> {{ $book->author}} </div>
-        <div class="col"> {{ $book->isbn }} </div>
-        <div class="col"> {{ $book->genre }} </div>
-        <div class="col"> {{ $book->publishing_year }} </div>
-        <div class="col"> {{ $book->pages }} </div> 
-        <div class="col"> {{ $book->price }} </div> 
-        <div class="col"> {{ $book->plot }} </div> 
+        <div class="col-6 mt-3">  <b>Autore:</b> {{ $book->author}} </div>
+        <div class="col-6 mt-3"> <b>Genere:</b> {{ $book->genre }} </div>
+        <div class="col-6 mt-3">  <b>ISBN:</b> {{ $book->isbn }} </div>
+        <div class="col-6 mt-3">  <b>Anno di pubblicazione:</b> {{ $book->publishing_year }} </div>
+        <div class="col-6 mt-3">  <b>N° pagine:</b> {{ $book->pages }} </div> 
+        <div class="col-6 mt-3">  <b>Prezzo:</b> €{{ $book->price }} </div> 
+        <div class="col-12 mt-3">  <b>Trama:</b> <br> {{ $book->plot }} </div> 
 
 </div>
 
