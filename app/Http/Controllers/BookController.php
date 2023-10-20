d86705232efc823cdfd1e572000c3daf1a94cdc1
robertoraciti <rob.raciti97@gmail.com>
2023-10-20 16:48:42 +0200

    added edit

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -59,11 +59,11 @@ class BookController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Book $book)
     {
-        //
+        return view('books.edit', compact('book'));
     }
 
     /**
@@ -71,7 +71,7 @@ class BookController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
---
+++ resources/views/books/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('main-content')
+    
+
+    <form action="{{route('books.update', $book)}}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{$book->title}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="author" class="form-label">Autore</label>
+            <input type="text" class="form-control" id="author" name="author" value="{{$book->author}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="isbn" class="form-label">ISBN</label>
+            <input type="number" class="form-control" id="isbn" name="isbn" value="{{$book->isbn}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="genre" class="form-label">Genere</label>
+            <input type="text" class="form-control" id="genre" name="genre" value="{{$book->genre}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="publishing_year" class="form-label">Anno pubblicazione</label>
+            <input type="number" class="form-control" id="publishing_year" name="publishing_year" value="{{$book->publishing_year}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="pages" class="form-label">Numero pagine</label>
+            <input type="number" class="form-control" id="pages" name="pages" value="{{$book->pages}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="price" class="form-label">Prezzo</label>
+            <input type="number" class="form-control" id="price" name="price" value="{{$book->price}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="plot" class="form-label">Trama</label>
+            <input type="text" class="form-control" id="plot" name="plot" value="{{$book->plot}}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Aggiungi</button>
+    </form>
+
+@endsection
