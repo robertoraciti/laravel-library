520a65cf1f2a6fbe7d6de19c73efc511b08001de
robertoraciti <rob.raciti97@gmail.com>
2023-10-24 17:14:31 +0200

    add error's messages create

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -1,52 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container mt-5">
+
+        @if ($errors->any())  
+        <div class="alert alert-danger">
+            Correggi i seguenti errori:
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>
+                    {{$error}}
+                </li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
     
-
-    <form action="{{route('admin.books.store')}}" method="POST">
-        @csrf
-
-        <div class="mb-3">
-            <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title">
-        </div>
-
-        <div class="mb-3">
-            <label for="author" class="form-label">Autore</label>
-            <input type="text" class="form-control" id="author" name="author">
-        </div>
-
-        <div class="mb-3">
-            <label for="isbn" class="form-label">ISBN</label>
-            <input type="number" class="form-control" id="isbn" name="isbn">
-        </div>
-
-        <div class="mb-3">
-            <label for="genre" class="form-label">Genere</label>
-            <input type="text" class="form-control" id="genre" name="genre">
-        </div>
-
-        <div class="mb-3">
-            <label for="publishing_year" class="form-label">Anno pubblicazione</label>
-            <input type="number" class="form-control" id="publishing_year" name="publishing_year">
-        </div>
-
-        <div class="mb-3">
-            <label for="pages" class="form-label">Numero pagine</label>
-            <input type="number" class="form-control" id="pages" name="pages">
-        </div>
-
-        <div class="mb-3">
-            <label for="price" class="form-label">Prezzo</label>
-            <input type="number" class="form-control" id="price" name="price">
-        </div>
-
-        <div class="mb-3">
-            <label for="plot" class="form-label">Trama</label>
-            <input type="text" class="form-control" id="plot" name="plot">
-        </div>
-
-        <button type="submit" class="btn btn-primary">Aggiungi</button>
-    </form>
-
-@endsection
+        <form action="{{route('admin.books.store')}}" method="POST">
+            @csrf
+    
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror " id="title" name="title" value="{{old('title')}}">
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="author" class="form-label">Autore</label>
+                <input type="text" class="form-control @error('author') is-invalid @enderror" id="author" name="author" value="{{old('author')}}">
+                @error('author')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="isbn" class="form-label">ISBN</label>
+                <input type="number" class="form-control @error('isbn') is-invalid @enderror" id="isbn" name="isbn" value="{{old('isbn')}}">
+                @error('isbn')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="genre" class="form-label">Genere</label>
+                <input type="text" class="form-control @error('genre') is-invalid @enderror" id="genre" name="genre" value="{{old('genre')}}">
+                @error('genre')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="publishing_year" class="form-label">Anno pubblicazione</label>
+                <input type="number" class="form-control @error('publishing_year') is-invalid @enderror" id="publishing_year" name="publishing_year" value="{{old('publishing_year')}}">
+                @error('publishing_year')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="pages" class="form-label">Numero pagine</label>
+                <input type="number" class="form-control @error('pages') is-invalid @enderror" id="pages" name="pages" value="{{old('pages')}}">
+                @error('pages')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{old('price')}}">
+                @error('price')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
+    
+            <div class="mb-3">
+                <label for="plot" class="form-label">Trama</label>
+                <textarea class="form-control" name="plot" id="plot" name="plot">{{old('plot')}}</textarea>
+            </div>
+    
+            <button type="submit" class="btn btn-primary">Aggiungi</button>
+        </form>
+    
+    @endsection
+    </div>
